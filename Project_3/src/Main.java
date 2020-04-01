1f4c836f988e597bfac07d8b6b4d0411cb06b6ad
Shindu <48678358+Shindu-dot@users.noreply.github.com>
2020-04-01 09:20:03 +0200

    Added args check

--- Project_3/src/Main.java
+++ Project_3/src/Main.java
@@ -1,5 +1,22 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO
+        // args
+        final double alpha;
+        final Path trainSetPath;
+
+        if (args.length == 2) {
+            alpha = Double.parseDouble(args[0]);
+            trainSetPath = Paths.get(args[1]);
+        } else {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("> Enter alpha:");
+            alpha = sc.nextDouble();
+            System.out.println("> Enter train set path:");
+            trainSetPath = Paths.get(sc.next());
+        }
     }
 }
