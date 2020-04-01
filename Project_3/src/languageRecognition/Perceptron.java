4c210307639d5046c5f4242f27e2dd5e85df9314
Shindu <48678358+Shindu-dot@users.noreply.github.com>
2020-04-01 15:05:20 +0200

    Added constructor to Perceptron and three extra .txt files about Van Gogh

--- Project_3/src/languageRecognition/Perceptron.java
+++ Project_3/src/languageRecognition/Perceptron.java
@@ -4,4 +4,8 @@ public class Perceptron {
     private double alpha;
     private double t; // theta
     private double[] w; // weights
+
+    public Perceptron(double alpha) {
+        this.alpha = alpha;
+    }
 }
