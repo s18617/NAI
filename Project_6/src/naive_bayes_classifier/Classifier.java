c3c0aea04112655705ab4249853d4c1e27f512eb
Shindu <48678358+Shindu-dot@users.noreply.github.com>
2020-05-06 09:56:02 +0200

    Classifier adjusted to multiple classes

--- Project_6/src/naive_bayes_classifier/Classifier.java
+++ Project_6/src/naive_bayes_classifier/Classifier.java
@@ -2,66 +2,80 @@ package naive_bayes_classifier;
 
 import javafx.util.Pair;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Classifier {
-    private final HashMap<String, Integer>[] valueCounts1;
-    private final HashMap<String, Integer>[] valueCounts2;
-    private int totalCount1 = 0; // number of observations classified as classification1
-    private int totalCount2 = 0; // number of observations classified as classification2
-    private int totalCount; // number of observations classified as classification1 or classification2
+    private HashMap<String, Integer>[][] valueCounts; // [classificationIndex][columnIndex]
+    private int totalCounts[]; // [classificationIndex]
+    private int totalCount;
 
-    private String classification1;
-    private String classification2;
+    private ArrayList<String> classifications = new ArrayList<>();
 
     @SuppressWarnings("unchecked")
     public Classifier(List<Pair<String[], String>> observations) {
+        final int columns = observations.get(0).getKey().length;
+
 
         // classifications
-        classification1 = observations.get(0).getValue();
-        for (int i = observations.size() - 1; i > 0; i--) {
-            if (!observations.get(i).getValue().equals(classification1)) {
-                classification2 = observations.get(i).getValue();
-                break;
+        classifications.add(0, observations.get(0).getValue());
+        for (Pair<String[], String> observation : observations) {
+            if (!classifications.contains(observation.getValue())) {
+                classifications.add(classifications.size(), observation.getValue());
             }
         }
 
 
         // value counts
-        valueCounts1 = new HashMap[observations.get(0).getKey().length];
-        valueCounts2 = new HashMap[observations.get(0).getKey().length];
-        for (int i = 0; i < valueCounts1.length; i++) {
-            valueCounts1[i] = new HashMap<>();
-            valueCounts2[i] = new HashMap<>();
+        valueCounts = new HashMap[classifications.size()][];
+        for (int i = 0; i < valueCounts.length; i++) {
+            valueCounts[i] = new HashMap[columns];
+            for (int j = 0; j < valueCounts[i].length; j++) {
+                valueCounts[i][j] = new HashMap<>();
+            }
         }
 
+
         // counting
-        for (int i = 0; i < observations.size(); i++) {
-            for (int j = 0; j < observations.get(i).getKey().length; j++) {
-                if (observations.get(i).getValue().equals(classification1)) {
-                    String key = observations.get(i).getKey()[j];
-                    int count = valueCounts1[j].getOrDefault(key, 0);
-                    valueCounts1[j].put(key, count + 1);
-                    totalCount1++;
-                } else {
-                    String key = observations.get(i).getKey()[j];
-                    int count = valueCounts2[j].getOrDefault(key, 0);
-                    valueCounts2[j].put(key, count + 1);
-                    totalCount2++;
+        totalCounts = new int[classifications.size()];
+        for (Pair<String[], String> observation : observations) {
+            // i - column index
+            for (int i = 0; i < observation.getKey().length; i++) {
+                // j - classification index
+                for (int j = 0; j < classifications.size(); j++) {
+                    if (classifications.get(j).equals(observation.getValue())) {
+                        String key = observation.getKey()[i];
+                        int count = valueCounts[j][i].getOrDefault(key, 0);
+                        valueCounts[j][i].put(key, count + 1);
+                        totalCounts[j]++;
+                    }
                 }
             }
         }
 
         // totalCount
-        totalCount = totalCount1 + totalCount2;
+        totalCount = IntStream.of(totalCounts).sum();
     }
 
     public void classify(String[] observation) {
-        
+
     }
 
     public void test(List<String[]> observations) {
 
+    }
+
+    private void printValueCounts() {
+        for (int i = 0; i < classifications.size(); i++) {
+            System.out.println("<<< " + classifications.get(i) + " >>>");
+            for (int j = 0; j < valueCounts[i].length; j++) {
+                System.out.println("< col " + j + " >");
+                for (String key : valueCounts[i][j].keySet()) {
+                    System.out.println(key + " : " + valueCounts[i][j].get(key));
+                }
+            }
+        }
     }
 }
