578c38cc8933e1d70df21f49621d239b5ccb8f43
Shindu <48678358+Shindu-dot@users.noreply.github.com>
2020-05-12 20:12:18 +0200

    Project_7 done

--- Project_7/src/Knapsack.java
+++ Project_7/src/Knapsack.java
@@ -1,3 +1,100 @@
-public class Knapsack {
+import javafx.util.Pair;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Knapsack {
+    private final double capacity; // knapsack capacity
+    private final ArrayList<Pair<Double, Double>> items; // <value, weight>
+
+    public Knapsack(double capacity, ArrayList<Pair<Double, Double>> items) {
+        this.capacity = capacity;
+        this.items = items;
+    }
+
+    public byte[] bruteForce() {
+        byte[] bestSelection = null;
+        double bestValue = -1;
+
+        for (int i = 1; i < Math.pow(2, this.items.size()); i++) {
+            // generating selection
+            String[] bin = Integer.toBinaryString(i).split("");
+            byte[] selection = new byte[this.items.size()];
+            for (int j = bin.length - 1; j >= 0; j--) {
+                selection[j + (selection.length - bin.length)] = Byte.parseByte(bin[j]);
+            }
+
+            // checking selection
+            double value = 0;
+            double weight = 0;
+
+            for (int j = 0; j < selection.length; j++) {
+                weight += items.get(j).getValue() * selection[j];
+                value += items.get(j).getKey() * selection[j];
+            }
+
+            if (weight <= capacity) {
+                if (value > bestValue) {
+                    bestValue = value;
+                    bestSelection = selection;
+                    System.out.println("New best selection " + Arrays.toString(bestSelection));
+                }
+            }
+        }
+
+        return bestSelection;
+    }
+
+    public static Knapsack loadKnapsack(File file) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            ArrayList<Pair<Double, Double>> items = new ArrayList<>();
+
+            String[] split = br.readLine().split(" ", 2);
+
+            double capacity = Double.parseDouble(split[0]);
+
+            String[] values = br.readLine().split(",");
+            String[] weights = br.readLine().split(",");
+
+            if (values.length != weights.length) {
+                throw new Exception("Invalid file format");
+            }
+
+            for (int i = 0; i < values.length; i++) {
+                items.add(i, new Pair<>(Double.parseDouble(values[i]), Double.parseDouble(weights[i])));
+            }
+
+            return new Knapsack(capacity, items);
+        } catch (IOException ex) {
+            System.err.println("> Error occurred while reading a file containing knapsack info");
+            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Capacity=").append(capacity)
+                .append("; ItemsCount=").append(items.size())
+                .append("; Items={");
+
+        for (int i = 0; i < items.size(); i++) {
+            sb.append("(v=").append(items.get(i).getKey()).append(", w=").append(items.get(i).getValue()).append(")");
+            if (i < items.size() - 1) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
--- Project_7/src/Main.java
+++ Project_7/src/Main.java
@@ -1,5 +1,58 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        File[] files = new File[]{
+                new File("knapsacks/knapsack.txt"),
+                new File("knapsacks/PO1.txt"),
+                new File("knapsacks/PO2.txt"),
+                new File("knapsacks/PO3.txt"),
+                new File("knapsacks/PO4.txt"),
+                new File("knapsacks/PO5.txt"),
+                new File("knapsacks/PO6.txt"),
+                new File("knapsacks/PO7.txt"),
+                new File("knapsacks/PO8.txt")
+        };
 
+        Knapsack[] knapsacks = new Knapsack[files.length];
+
+        for (int i = 0; i < files.length; i++) {
+            knapsacks[i] = Knapsack.loadKnapsack(files[i]);
+        }
+
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("> Enter index of chosen knapsack (or 'q' to quit)");
+        printIndexedKnapsacks(knapsacks);
+        String input = sc.nextLine();
+
+        if (input.equals("q")) {
+            System.exit(0);
+        }
+
+        while (running) {
+            byte[] selection = knapsacks[Integer.parseInt(input)].bruteForce();
+            System.out.println("> Best selection is " + Arrays.toString(selection));
+
+            System.out.println("\r\n> Enter index of chosen knapsack (or 'q' to quit)");
+            printIndexedKnapsacks(knapsacks);
+            input = sc.nextLine();
+
+            if (input.equals("q")) {
+                running = false;
+            }
+        }
+
+
+        sc.close();
+    }
+
+    private static void printIndexedKnapsacks(Knapsack[] knapsacks) {
+        for (int i = 0; i < knapsacks.length; i++) {
+            System.out.println("[" + i + "] " + knapsacks[i].toString());
+        }
     }
 }
