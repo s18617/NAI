794a6a4ad40d1f774f32a8fd3123f528e53eacd5
s18617 <s18617@pjwstk.edu.pl>
2021-02-20 21:57:44 +0100

    removed redundant folders

--- Project_8/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        TravellingSalesmanProblem tsp = new TravellingSalesmanProblem(5, new double[][]{});
-        System.out.println(Arrays.toString(tsp.getRandomTour()));
-    }
-}
